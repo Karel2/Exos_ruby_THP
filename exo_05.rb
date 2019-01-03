b52e4421b473f11124d74bfce198d20e54d47dcc
Karel <cloareckarel@gmail.com>
2019-01-03 20:05:53 +0100

    Exos Ruby Jeudi

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #Affiche du texte
+puts "travail : #{10 * 5 * 11}" #Affiche du texte avec un calcul grâce au #{}
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #Affiche du texte avec un calcul
+
+puts "Et en secondes ?" #Affiche du texte
+
+puts 10 * 5 * 11 * 60 * 60 #Affiche un calcul qui s'effectue car sans ""
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #Montre que les calculs dans les "" ne s'effectuent pas
+
+puts 3 + 2 < 5 - 7 #Affiche une condition True/False 5 < -2
+
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" #Affiche du texte avec un calcul
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" #Affiche du texte avec un calcul
+
+puts "Ok c'est faux alors !" #Affiche du texte
+
+puts "C'est drôle ça, faisons-en plus :" #Affiche du texte
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #Affiche du texte avec une condition qui s'effectue dans #{}
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #Affiche du texte avec une condition qui s'effectue dans #{}
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #Affiche du texte avec une condition qui s'effectue dans #{}
---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Salut c'est quoi ton prénom ?"
+user_prénom = gets.chomp
+puts "Et ton joli nom ?"
+user_nom = gets.chomp
+puts "Bonjour, " + user_prénom + " " + user_nom + " !"
