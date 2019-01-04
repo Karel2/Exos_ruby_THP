8b728374f9f1492e66d382cb4f33aef81be59e87
Karel <cloareckarel@gmail.com>
2019-01-04 20:17:19 +0100

    Amélioration pyramides

--- exo_20.rb
+++ exo_20.rb
@@ -1,6 +1,6 @@
 puts "Entrez un nombre entre 1 et 25 : "
 n = gets.chomp.to_i
-if n >= 25
+if n > 25
   	puts "Votre nombre n’est pas correct."
 elsif n <= 0
 	puts "Votre nombre n’est pas correct."
--- pyramide.rb
+++ pyramide.rb
@@ -1,14 +1,14 @@
 puts "Entrez un nombre entre 1 et 25 : "
 n = gets.chomp.to_i
-if n >= 25
+if n > 25
   	puts "Votre nombre n’est pas correct."
 elsif n <= 0
 	puts "Votre nombre n’est pas correct."
 else
 def pyramid(height)
-  height.times {|n|
-    print '  ' * (height - n)
-    puts '#' * (2 * n + 1)
+  height.times {|i|
+    print '  ' * (height - i)
+    puts '#' * (2 * i + 1)
   }
 end
 end
